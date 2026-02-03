0ac38993832644c86e40412607b4905f69867ef8
MaheshKotambe <technomk99@gmail.com>
2026-02-03 19:27:33 +0530

    next commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+// mongoose.connect('mongodb:127.0.0.1:27017/booksdb',{
+
+// }).then(()=>{
+//     console.log('MongoDB connected');
+// }).catch((err)=>{
+//     console.error('MongoDB connection error : ', err);
+// });
+
+app.get('/', (req,res)=>{
+    res.send('backend is running');
+});
+
+const PORT = 5000;
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+});
