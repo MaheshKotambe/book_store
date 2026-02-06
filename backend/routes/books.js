fbeee4b091c44a6b1126a115969fa45d0192d7bc
MaheshKotambe <technomk99@gmail.com>
2026-02-06 11:13:40 +0530

    next commit

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-const book = require('../models/Book');
+const Book = require('../models/Book');
 
 //CRUD api routes
 
@@ -16,6 +16,39 @@ router.post('/', async(req,res) => {
         res.status(400).json({error: err.message});
     }
 });
+
+//fetch all books
+router.get('/', async(req,res) => {
+    try{
+        const books = await Book.find();
+        res.json(books);
+    }
+    catch(err){
+        res.status(500).json({error: err.message});
+    }
+});
+
+//update book
+router.put('/:id', async(req,res) => {
+    try{
+        const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, {new:true});
+        res.json(updatedBook);
+    }
+    catch(err){
+        res.status(400).json({error: err.message});
+    }
+})
+
+//delete book
+router.delete('/:id', async(req,res) => {
+    try{
+        await Book.findByIdAndDelete(req.params.id);
+        res.json({message: 'book deleted successfully'});
+    }
+    catch(err){
+        res.status(500).json({error: err.message});
+    }
+})
 
 module.exports = router;
 
