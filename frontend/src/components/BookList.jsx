4e4068d3f2c85f26d7bb083807211e64730e1e19
MaheshKotambe <technomk99@gmail.com>
2026-02-06 19:21:40 +0530

    next commit

--- frontend/src/components/BookList.jsx
+++ frontend/src/components/BookList.jsx
@@ -1,7 +1,24 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const BookList = ()=>{
+const BookList = () => {
+
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const fetchBooks = async () => {
+        try {
+            const res = await axios.get('http://127.0.0.1:5000/api/books/');
+            setBooks(res.data);
+        }
+        catch (err) {
+            console.log("Error fetching books : ", err);
+        }
+    }
+
     return (
         <div className='container mt-5'>
             <h2 className='text-center mb-4'>Book List</h2>
@@ -15,12 +32,21 @@ const BookList = ()=>{
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Mern stack book</td>
-                        <td>abcd efg</td>
-                        <td>2000</td>
-                        <td>900</td>
-                    </tr>
+                    {books.length > 0 ? (
+                        books.map((book) => (
+                            <tr>
+                                <td>{book.title}</td>
+                                <td>{book.author}</td>
+                                <td>{book.year}</td>
+                                <td>{book.price}</td>
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td>No books found</td>
+                        </tr>
+                    )}
+
                 </tbody>
             </table>
         </div>
